a87c1390ad18eb4fd9bcfdf368dcc3bd8674a76b
Abdallah Awwad <ar.awwad6@gmail.com>
2024-11-09 17:43:12 +0100

    handle missing commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		command := strings.TrimSpace(input)
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+	}
+
 }
