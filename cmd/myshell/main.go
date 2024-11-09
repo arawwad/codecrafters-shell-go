bdb683f68725be146e6a4f2f5775d165c05731ca
Abdallah Awwad <ar.awwad6@gmail.com>
2024-11-09 19:26:23 +0100

    extend type command to find executables within path

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,37 +16,70 @@ var builtins = []string{
 
 func main() {
 	for {
-		fmt.Fprint(os.Stdout, "$ ")
+		prompt()
 
-		// Wait for user input
-		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
+		command, args, ok := getInput()
+
+		if !ok {
 			continue
 		}
-		inputParts := strings.Fields(strings.TrimSpace(input))
-		if len(inputParts) == 0 {
-			continue
-		}
-		command := inputParts[0]
-		args := inputParts[1:]
+
 		if command == "exit" {
-			os.Exit(0)
-			continue
+			exitCommand()
 		}
 		if command == "echo" {
-			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(args, " "))
+			echoCommand(args)
 			continue
 		}
 		if command == "type" {
-			if slices.Contains(builtins, args[0]) {
-				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", args[0])
-			} else {
-				fmt.Fprintf(os.Stdout, "%s: not found\n", args[0])
-			}
+			typeCommand(args[0])
 			continue
 		}
 		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
-
 	}
+}
+
+func prompt() {
+	fmt.Fprint(os.Stdout, "$ ")
+}
+
+func getInput() (string, []string, bool) {
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return "", nil, false
+	}
+	inputParts := strings.Fields(strings.TrimSpace(input))
+	if len(inputParts) == 0 {
+		return "", nil, false
+	}
+	return inputParts[0], inputParts[1:], true
+}
+
+func exitCommand() {
+	os.Exit(0)
+}
+
+func echoCommand(args []string) {
+	fmt.Fprintf(os.Stdout, "%s\n", strings.Join(args, " "))
+}
+
+func typeCommand(commandName string) {
+
+	paths := strings.Split(os.Getenv("PATH"), ":")
+
+	if slices.Contains(builtins, commandName) {
+		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", commandName)
+		return
+	}
+	for _, path := range paths {
+		pathFiles, _ := os.ReadDir(path)
+		for _, dirEntry := range pathFiles {
+			if dirEntry.Name() == commandName {
+				fmt.Fprintf(os.Stdout, "%s is %s/%s\n", commandName, path, commandName)
+				return
+			}
+		}
+	}
+	fmt.Fprintf(os.Stdout, "%s: not found\n", commandName)
 }
